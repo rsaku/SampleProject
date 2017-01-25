808954f8332223ee5ef01ccf4aaf6fa8693e74fb
R.Sakunaga <ryuji.sakunaga@proaxia-consulting.co.jp>
2017-01-25 16:32:15 +0900

    first

---
+++ WebApplication1/WebApplication1/Controllers/HelloWorldController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Wellcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "ハロー " + name;
+            ViewBag.NumTimes = numTimes;
+            return View();
+        }
+
+        /*
+                public string Index()
+                {
+                    return "Hello World・・・";
+
+                }
+
+        public string Wellcome(string name, int numTimes = 1)
+        {
+            return HttpUtility.HtmlEncode("ここはweeeeeeelcome：ハロー " + name + "さん。 numTimes = " + numTimes);
+        }
+         */
+
+        public string Login(string name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + "  ID:" + ID);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    // 映画
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name="リリース日")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        public string Genre { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+    }
+
+    public class MovieDBContext:DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
